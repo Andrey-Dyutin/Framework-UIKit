202290b953cf13aac50bb99f84523cfee19ece62
Andrey-Dyutin <55854890+Andrey-Dyutin@users.noreply.github.com>
2020-08-09 09:39:15 +0400

    Commit 1

--- UIKit 4 - UITextField/ViewController.swift
+++ UIKit 4 - UITextField/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func donePressed(_ sender: UIButton) {
+        guard textField.text?.isEmpty == false else { return }
+        
+        if let _ = Double(textField.text!) {
+            
+            let alert = UIAlertController(title: "Wrong format", message: "Please enter your name", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+            
+        } else {
+            
+            label.text = textField.text
+            textField.text = nil
+            
+        }
+    }
+    
 }
 
