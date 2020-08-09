0373e361d2714b4c0f0d4ec678b0297ecbdcb12a
Andrey-Dyutin <55854890+Andrey-Dyutin@users.noreply.github.com>
2020-08-09 08:05:50 +0400

    Initial Commit

---
+++ UIKit 2 - UISegmentedControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKit 2 - UISegmentedControl
+//
+//  Created by Андрей Дютин on 09.08.2020.
+//  Copyright © 2020 Андрей Дютин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
