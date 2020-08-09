5d31307d34202680b0f825ea6187b671f3ef012d
Andrey-Dyutin <55854890+Andrey-Dyutin@users.noreply.github.com>
2020-08-09 09:11:01 +0400

    Commit 1

--- UIKit 3 - UISlider/ViewController.swift
+++ UIKit 3 - UISlider/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        slider.value = 1
+        label.text = String(slider.value)
+        label.textAlignment = .center
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .black
     }
 
-
+    @IBAction func sliderAction(_ sender: UISlider) {
+        label.text = String(sender.value)
+        
+        let backgrandColor = self.view.backgroundColor
+        self.view.backgroundColor = backgrandColor?.withAlphaComponent(CGFloat(sender.value))
+    }
+    
 }
 
